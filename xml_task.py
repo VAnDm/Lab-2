c9b39d129f3398f8a6e5f094922299bf5df2c0c3
VAnDm <klirens32@gmail.com>
2025-01-17 00:37:18 +0300

    xml_task

---
+++ xml_task.py
@@ -0,0 +1,30 @@
+"""
+Программа создает словарь
+числовой код валюты - название валюты
+словарь выводится в виде списка кортежей
+(ключ, значение)
+"""
+import xml.dom.minidom
+
+
+xml_file = open('currency.xml')
+xml_data = xml_file.read()
+
+dom = xml.dom.minidom.parseString(xml_data)
+dom.normalize()
+
+currencies = dom.getElementsByTagName('Valute')
+currency_dict = {}
+for currency in currencies:
+    name = ''
+    number = ''
+    for attribute in currency.childNodes:
+        if attribute.tagName == "CharCode":
+            name = attribute.firstChild.data
+        if attribute.tagName == "NumCode":
+            number = attribute.firstChild.data
+    currency_dict[number] = name
+currency_dict_list = []
+for i in currency_dict:
+    currency_dict_list.append((i, currency_dict[i]))
+print(*currency_dict_list)
--- xml_tasks.py
+++
@@ -1,7 +0,0 @@
-import xml.dom.minidom as minidom
-
-
-xml_file = open('currency.xml')
-xml_data = xml_file.read()
-
-xml_dict = 
